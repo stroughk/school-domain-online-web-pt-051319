2db24aed01004d7737d8bae58a48d3efa154e2f7
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-27 23:34:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,14 @@
 class School 
   
-  attr_accessor (:roster)
   
   def initialize(name)
     @name = name 
   end
   
+  def add_student(name, grade)
+    @name = name
+    @grade=grade 
+  end
   
   
 end
