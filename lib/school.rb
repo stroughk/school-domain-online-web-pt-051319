08d8200e4950f780e22ff81fbe73d7fb9147f884
Kateryna Strough <katerynastrough@hotmail.com>
2019-05-27 23:32:48 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,12 @@
 class School 
   def initialize(name)
     @name = name 
+    @roster {} 
   end
   
-  def roster
-    roster{}
-  end
+  
   
 end
 
 school = School.new("Bayside High School")
+school.roster 
